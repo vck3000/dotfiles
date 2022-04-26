f868be91952af9338eecaace31e159381bc2775e
Victor Kuo <vck3000@gmail.com>
2022-04-26 21:21:36 +1000

    Synchronously install treesitter dependencies for easier bootstrapping.

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,6 +1,6 @@
 require('nvim-treesitter.configs').setup({
   ensure_installed = 'all',
-  sync_install = false,
+  sync_install = true,
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = false,
